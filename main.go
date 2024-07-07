75e825b3403620f697c3fa59d12febddd4bee6d9
Sandhya Madne <shinde.sandhya2993@gmail.com>
2024-07-07 12:22:08 -0400

    Create response struct

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+type spamResponse struct {
+	MethodName  string `json:"method_name"`
+	AuthKey     string `json:"auth_key"`
+	SenderEmail string `json:"sender_email"`
+	SenderIp    string `json:"sender_ip"`
+	JsOn        int    `json:"js_on"`
+	SubmitTime  int    `json:"submit_time"`
+}
+
 func main() {
 	fmt.Println("Init checks")
 }
