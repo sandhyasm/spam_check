001e3326f93c0955f6b853beac92e8bd428f4422
Sandhya Madne <shinde.sandhya2993@gmail.com>
2024-07-07 12:45:46 -0400

    Create request object

--- main.go
+++ main.go
@@ -3,18 +3,39 @@ package main
 import "fmt"
 
 type spamRequest struct {
-	MethodName  string `json:"method_name"`
-	AuthKey     string `json:"auth_key"`
-	SenderEmail string `json:"sender_email"`
-	SenderIp    string `json:"sender_ip"`
-	JsOn        int    `json:"js_on"`
-	SubmitTime  int    `json:"submit_time"`
+	MethodName     string `json:"method_name"`
+	AuthKey        string `json:"auth_key"`
+	SenderEmail    string `json:"sender_email"`
+	SenderNickname string `json:"sender_nickname"`
+	SenderIp       string `json:"sender_ip"`
+	JsOn           int    `json:"js_on"`
+	SubmitTime     int    `json:"submit_time"`
 }
 
 type spamResponse struct {
-	Data []
+	Data map[string]interface{} `json:"data"`
 }
 
 func main() {
+
+	/*
+		data='{"method_name":"check_message",
+		"auth_key":"your_acccess_key",
+		"sender_email":"stop_email@example.com",
+		"sender_nickname":"John Doe",
+		"sender_ip":"127.0.0.1",
+		"js_on":1,"submit_time":15}' https://moderate.cleantalk.org/api2.0
+	*/
+
+	request := spamRequest{
+		MethodName:     "check_message",
+		AuthKey:        "nysumygepuvetud",
+		SenderEmail:    "abc@test.com",
+		SenderNickname: "Abc Test",
+		SenderIp:       "120.18.17.10",
+		JsOn:           1,
+		SubmitTime:     15,
+	}
+
 	fmt.Println("Init checks")
 }
