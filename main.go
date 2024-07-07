d5f0a2c037b81c1b15a5c1cf6fbac265f91d0b66
Sandhya Madne <shinde.sandhya2993@gmail.com>
2024-07-07 12:31:22 -0400

    Rename request and response struct

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 
-type spamResponse struct {
+type spamRequest struct {
 	MethodName  string `json:"method_name"`
 	AuthKey     string `json:"auth_key"`
 	SenderEmail string `json:"sender_email"`
 	SenderIp    string `json:"sender_ip"`
 	JsOn        int    `json:"js_on"`
 	SubmitTime  int    `json:"submit_time"`
+}
+
+type spamResponse struct {
+	Data []
 }
 
 func main() {
